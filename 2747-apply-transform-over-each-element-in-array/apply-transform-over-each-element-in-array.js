806037b5fb29bc1eacff57cfded009f5f111c9af
Niranjan M Nadiger <nadigerniranjan@gmail.com>
2026-09-22 00:04:11 +0530

    Time: 52 ms (11.13%) | Memory: 53.8 MB (37.41%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -1,13 +1,8 @@
-/**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
- */
 var map = function (arr, fn) {
-    const returnedArray = [];
+    const returnedArray = new Array(arr.length);
 
     for (let i = 0; i < arr.length; i++) {
-        returnedArray.push(fn(arr[i], i));
+        returnedArray[i] = fn(arr[i], i);
     }
 
     return returnedArray;
